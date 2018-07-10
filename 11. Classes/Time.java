d7212d0d44572204ee2fc1cb8ce7ddb3754d19e9
nchong128 <nchong128@gmail.com>
2018-07-10 11:12:49 +0800

    Done all  theory for chapter 11

--- 11. Classes/Time.java
+++ 11. Classes/Time.java
@@ -88,7 +88,7 @@ public class Time {
     }
 
     // Setters (mutators) are methods that modify the attributes of a class
-    // Conventino si for method setSomething to change that something
+    // Convention is for method setSomething to change that something
     public void setHour(int hour) {
         this.hour = hour;
     }
@@ -117,7 +117,80 @@ public class Time {
     // Similar to the printTime method except this is used to refer to the current instance
         // and String.format is used to return a formatted String without displaying
     
+    /** equals Method
+        - Have seen two ways to check if values are equal, using == or equals method
+        - == operator checks whether objects are identical, i.e. whether they are the same object
+        - equals method checks whether objects are equivalent, i.e. whether they have the same value
+        - Definition of identity is always the same but not for equivalence -> Objects can define their
+            - own equals method
+        - By default, the equals method does the same thing as ==, need to redefine
+        - equals is an instance method, uses this to refer to the current object and does have the static keyword
+     */
     
+    public boolean equals(Time that) {
+        return this.hour == that.hour
+            && this.minute == that.minute
+            && this.second == that.second;
+    }
 
-    
+    /** Adding Times
+        - Two ways to add time:
+            - Can write a static metho that takes two Time objects as parameters
+            - Can write an instance method that gets invoked on one object and takes the other as a parameter
+     */
+
+    // Static approach
+    public static Time add(Time t1, Time t2) {
+        Time sum = new Time();
+        sum.hour = t1.hour + t2.hour;
+        sum.minute = t1.minute + t2.minute;
+        sum.second = t1.second + t2.second;
+        return sum;
+    }
+
+    // Note: no static keyword, no t1 parameter, t1 replaced with this
+    public Time add(Time t2) {
+        Time sum = new Time();
+        sum.hour = this.hour + t2.hour;
+        sum.minute = this.minute + t2.minute;
+        sum.second = this.second + t2.second;
+
+        if (sum.second >= 60.0) {
+            sum.second -= 60.0;
+            sum.minute += 1;
+        }
+        if (sum.minute >= 60) {
+            sum.minute -= 60;
+            sum.hour += 1;
+        }
+        return sum;
+    }
+
+    // Previous implementation does not modify either of its parameters,
+        // creates and returns a new Time object
+    // Can write a method like this that modifies the existing Time object
+    public void increment(double seconds) {
+        this.second += seconds;
+        while (this.second >= 60.0) {
+            this.second -= 60.0;
+            this.minute += 1;
+        }
+        while (this.minute >= 60) {
+            this.minute -= 60;
+            this.hour += 1;
+        }
+    }
+
+    /**
+        - Methods like add are pure because
+            - they do not modify the parameters
+            - they don't have an side effects (e.g. printing)
+            - the return value only depends on the parameters, not on any other state
+        - This is known as a modifier
+            - Usually void methods, sometimes returning a refrence to the object they modify
+            - More efficient as no new objects created but more error-prone
+            - When objects are aliased, the effects of modifiers can be confusing
+        - To make a class immutable, can provide getters but no setters
+            - and pure methods but no modifiers
+     */    
 }
--- 11. Classes/TimeClient.java
+++ 11. Classes/TimeClient.java
@@ -2,9 +2,15 @@ public class TimeClient {
 
     public static void main(String[] args) {
         Time time = new Time(11, 59, 59.5);
+        
+        Time startTime = new Time(18,50,0.0);
+        Time runningTime = new Time(2,16,0.0);
+        Time endTime = Time.add(startTime, runningTime);
 
+        System.out.println(endTime);
 
-
+        Time endTime2 = startTime.add(runningTime);
+        System.out.println(endTime2);
     }
     
 
